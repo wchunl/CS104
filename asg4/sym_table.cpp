fcde2444e6901a419a212ba302d11ac7b0dfccc2
Shineng <stang38@ucsc.edu>
2019-05-22 23:00:17 -0700

    basic traversal

--- asg4/sym_table.cpp
+++ asg4/sym_table.cpp
@@ -1,5 +1,8 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "sym_table.h"
+#include "astree.h"
+#include "lyutils.h"
 
 // symbol_table
 
@@ -21,5 +24,16 @@
  */
 
 void traverse(astree* root) {
-   // printf()
+    if(root == NULL){
+        return;
+    }
+   for (astree* child: root->children) {
+       traverse(child);
+   }
+   printf("|   ");
+   printf ( "%s \"%s\" (%zd.%zd.%zd)\n",
+            parser::get_tname (root->symbol), root->lexinfo->c_str(),
+            root->lloc.filenr, root->lloc.linenr, root->lloc.offset);
+
 }
+
